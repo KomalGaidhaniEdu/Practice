770d2696bab03535bcf3c3af1f53c53d1c3adbee
Komal Gaidhani Edu <komalgaidhani.edu@gmail.com>
2024-01-19 09:01:54 +0530

    String reverse

--- DSA/src/ReverseString.java
+++ DSA/src/ReverseString.java
@@ -8,5 +8,21 @@ public class ReverseString {
             reverse = reverse + str.charAt(i);
         }
         System.out.println("Reversed String: " + reverse);
+        System.out.println("Reversed String: " + reverseStringWithStringBuilder("Java"));
+        System.out.println("Reversed String: " + reverseStringWithStringBuilderWithBuiltInReverseFunction("GeeksForGeeks"));
+    }
+
+    public static String reverseStringWithStringBuilder(String str) {
+        StringBuilder reverse = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            System.out.println("i: " + i + "Char: " + str.charAt(i));
+            reverse.append(str.charAt(i));
+        }
+        return reverse.toString();
+    }
+
+    public static String reverseStringWithStringBuilderWithBuiltInReverseFunction(String str) {
+        StringBuilder stringBuilder = new StringBuilder(str);
+        return stringBuilder.reverse().toString();
     }
 }
