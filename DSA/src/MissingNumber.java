9b8f63cef2d7ab7b9919a017ffb68a005169f3fb
Komal Gaidhani Edu <komalgaidhani.edu@gmail.com>
2024-01-22 09:41:05 +0530

    Find missing number in an array

---
+++ DSA/src/MissingNumber.java
@@ -0,0 +1,40 @@
+//Write a Java program to find the missing number in an array of integers from 1 to n
+public class MissingNumber {
+    // Array of integers 1 to n --> cyclic sort
+
+    //Step 1:  sort array
+    private static int[] sortArray(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int j = arr[i]-1;
+            if (arr[i] < arr.length && arr[i] != j )
+                swap(arr, i, j);
+            else i++;
+        }
+
+        return arr;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // Step 2: Find Missing number --> if arr[index] != index + 1
+    private static int findMissingNumber(int[] sortedArray) {
+        for (int i = 0; i < sortedArray.length; i++) {
+            if (sortedArray[i] != i + 1)
+                return i + 1;
+        }
+        return sortedArray.length;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 6, 2, 1, 7}; // 4 is missing
+        int[] sortedArray = sortArray(arr);
+        int missingNumber = findMissingNumber(sortedArray);
+        System.out.println("Missing Number is " + missingNumber);
+    }
+
+}
--- DSA/src/Palindrome.java
+++ DSA/src/Palindrome.java
@@ -1,4 +1,4 @@
-public class PalindroneString {
+public class Palindrome {
 
     public static void main(String[] args) {
         isPalindromeString("madam");
