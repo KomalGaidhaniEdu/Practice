8e342042707b0cd30a5b7fce4ef30af7987db199
Komal Gaidhani Edu <komalgaidhani.edu@gmail.com>
2024-01-22 11:25:28 +0530

    Find If strings are Anagrams or not

--- DSA/src/Anagrams.java
+++ DSA/src/Anagrams.java
@@ -1,5 +1,35 @@
+import java.util.HashMap;
+
 public class Anagrams {
+
+    public static boolean anagrams(String str1, String str2) {
+
+        if (str1.length() != str2.length()) {
+            return false;
+        } else {
+            HashMap<Character, Integer> map = new HashMap<>();
+            for (int i = 0; i < str1.length(); i++) {
+                if (map.containsKey(str1.charAt(i)))
+                    map.put(str1.charAt(i), map.get(str1.charAt(i)) + 1);
+                else map.put(str1.charAt(i), 1);
+            }
+
+            for (int i = 0; i < str2.length(); i++) {
+                if (map.containsKey(str2.charAt(i)))
+                    map.put(str1.charAt(i), map.get(str1.charAt(i)) - 1);
+                else return false;
+            }
+
+            for (Character key : map.keySet()) {
+                if (map.get(key) != 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(anagrams("listen", "silent"));
+
     }
 }
